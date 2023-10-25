85d62775841f0f1aafdcb3bf8324f0cc2f3c4308
collins maina <collinsmaina1921@gmail.com>
2023-10-25 13:03:50 +0300

    First Commit

---
+++ app/src/main/java/com/fire1/collins/models/Post.kt
@@ -0,0 +1,9 @@
+package com.fire1.collins.models
+
+data class Post(
+    val title:String?=null,
+    val description:String?=null,
+    val timestamp :Long?=null,
+    val image:String?=null,
+    val postedby:String?=null
+)
